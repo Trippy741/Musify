b5852315c7aae6695a223698df7790f1b1b1a86d
Trippy741 <trippy741@gmail.com>
2022-01-11 18:03:50 +0200

    working on title + image

--- app/src/main/java/com/example/loginpage/CurrentSongPlayingFragment.java
+++ app/src/main/java/com/example/loginpage/CurrentSongPlayingFragment.java
@@ -1,6 +1,7 @@
 package com.example.loginpage;
 
 import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.LayoutInflater;
@@ -14,6 +15,15 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.gms.common.util.IOUtils;
+import com.squareup.picasso.Picasso;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
 
 
 //import com.google.android.exoplayer2.source.ProgressiveMediaSource;
@@ -73,7 +83,48 @@ public class CurrentSongPlayingFragment extends Fragment {
     }
     private void startPlayer()
     {
+        String URL = "https://www.youtube.com/watch?v=fahcCePs9O0&list=RDfahcCePs9O0&start_radio=1&ab_channel=Koma";
         MusicService musicService = new MusicService();
-        musicService.initPlayer(getContext(),"https://www.youtube.com/watch?v=fahcCePs9O0&list=RDfahcCePs9O0&start_radio=1&ab_channel=Koma");
+        musicService.initPlayer(getContext(),URL);
+
+        Picasso.with(getContext()).load("https://img.youtube.com/vi/"+getImageUrlQuietly(URL)+"/maxresdefault.jpg").into(songImage);
+        songName.setText(getTitleQuietly(URL));
+    }
+    public static String getImageUrlQuietly(String youtubeUrl) {
+        try {
+            if (youtubeUrl != null) {
+                return String.format("http://img.youtube.com/vi/%s/0.jpg", Uri.parse(youtubeUrl).getQueryParameter("v"));
+            }
+        } catch (UnsupportedOperationException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static String getTitleQuietly(String youtubeUrl) {
+        try {
+            if (youtubeUrl != null) {
+                URL embededURL = new URL("http://www.youtube.com/oembed?url=" + youtubeUrl + "&format=json");
+                return new JSONObject(IOUtils.toString(embededURL)).getString("title");
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+    public static String getAuthorQuietly(String youtubeUrl) { //Finish this please!!!!!!!!
+        try {
+            if (youtubeUrl != null) {
+                URL embededURL = new URL("http://www.youtube.com/oembed?url=" + youtubeUrl + "&format=json");
+                return new JSONObject(IOUtils.toString(embededURL)).getString("title");
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }
